73be1313974d4fac7604c1eff91d7dbb52a15456
IvelinStanchev <telerika123@gmail.com>
2014-08-25 12:40:36 +0300

    TreesTraversals

---
+++ TreesAndTraversals/2-TraverseADirectory/TraverseADirectory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_TraverseADirectory
+{
+    class TraverseADirectory
+    {
+        private static List<string> files = new List<string>();
+
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Program Files (x86)";
+            string fileExtension = "*.exe";
+            Console.WriteLine("The files are being traversed");
+            TraverseDirectory(rootPath, fileExtension);
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        private static void TraverseDirectory(string currentPath, string fileExtension)
+        {
+            string[] currentDirFiles = Directory.GetFiles(currentPath, fileExtension);
+            files.AddRange(currentDirFiles);
+
+            string[] curretDirDirectories = Directory.GetDirectories(currentPath);
+            foreach (var dir in curretDirDirectories)
+            {
+                TraverseDirectory(dir, fileExtension);
+            }
+        }
+    }
+}
